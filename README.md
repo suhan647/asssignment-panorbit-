b34e50d090fe9aedbb85c498286ea351af1c8131
suhan647 <suhanahmed647@gmail.com>
2023-03-08 14:47:27 +0530

    Routing Added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # asssignment-panorbit-
+
+# dependencies
+
+npm i react-router-dom
+npm install @mui/material @emotion/react @emotion/styled
+
--- assignment-panorbit/src/App.js
+++ assignment-panorbit/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { Route, Routes} from 'react-router-dom'
+import LandingPage from './components/landingpage/LandingPage'
+// import Sidebar from './components/sidebar/SideBar'
+import ProfileDetails from './components/pages/ProfileDetails'
+import PostsDetails from './components/pages/PostsDetails'
+import ToDoScreen from './components/pages/ToDoScreen'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello </h1>
-    </div>
+   <>
+   {/* <Sidebar /> */}
+   <Routes>
+    <Route path='/' element={<LandingPage />} />
+    <Route path='/profiledetails' element={<ProfileDetails />} />
+    <Route path='/postdetails' element={<PostsDetails />} />
+    <Route path='/todo' element={<ToDoScreen />} />
+   </Routes>
+
+   </>
   );
 }
 
