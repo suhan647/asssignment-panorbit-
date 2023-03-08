8b0404f5a25cc9966e386ae8900ef381b225d377
suhan647 <suhanahmed647@gmail.com>
2023-03-08 15:55:42 +0530

    Designed landingPage

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 npm i react-router-dom
 npm install @mui/material @emotion/react @emotion/styled
-
+npm i axios
--- assignment-panorbit/src/components/landingpage/LandingPage.js
+++ assignment-panorbit/src/components/landingpage/LandingPage.js
@@ -1,8 +1,85 @@
 import React from 'react'
+import '../../App.css'
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box'
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
+
 
 function LandingPage() {
   return (
-    <div>LandingPage</div>
+   <>
+   <div className='background_img'>
+  <div className='card_div'>
+   <Card sx={{ width:'400px', borderRadius:'20px' }}>
+      <CardContent>
+
+        <Box sx={{backgroundColor:'#D3D3D3', height:"10vh", alignItems:'center', display:"flex", justifyContent:'center'}}>
+        <Typography sx={{ fontSize: 18, display:'flex', justifyContent:"center"}} color="text.secondary" gutterBottom>
+           Select An Account        
+        </Typography>
+        </Box>
+        
+        <Box className='users_list'>
+        <Stack direction="column" spacing={2}>
+
+      <span className='avatar'>
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <p style={{padding:'15px'}}>Lean Graham</p>
+        </span>
+        <Divider />
+
+        <span className='avatar'>
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <p style={{padding:'15px'}}>Lean Graham</p>
+        </span>
+        <Divider />
+        
+        <span className='avatar'>
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <p style={{padding:'15px'}}>Lean Graham</p>
+        </span>
+        <Divider />
+
+        <span className='avatar'>
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <p style={{padding:'15px'}}>Lean Graham</p>
+        </span>
+        <Divider />
+
+        <span className='avatar'>
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <p style={{padding:'15px'}}>Lean Graham</p>
+        </span>
+        <Divider />
+
+        <span className='avatar'>
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <p style={{padding:'15px'}}>Lean Graham</p>
+        </span>
+        <Divider />
+
+        <span className='avatar'>
+        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        <p style={{padding:'15px'}}>Lean Graham</p>
+        </span>
+        <Divider />
+      
+    </Stack>
+        </Box>
+
+      </CardContent>
+      
+    </Card>
+    </div>
+
+
+   </div>
+
+   </>
   )
 }
 
